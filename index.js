2eaf6a01d8bcfdcb34283985e0777f7d92fd6d65
abdul basit <abdulbasit2745@gmail.com>
2025-08-03 16:00:29 +0500

    change

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 "use strict";
-console.log("abdul basit");
+console.log("string kaya ha");
+console.log("string ik text hota ha. jo haroof,alfaz,jumlay ya symbals ho skaty hen.jo bhi cheez quotes k andar likhi jaye string kehlati ha");
--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
-console.log("abdul basit")
+console.log("string kaya ha");
+console.log ("string ik text hota ha. jo haroof,alfaz,jumlay ya symbals ho skaty hen.jo bhi cheez quotes k andar likhi jaye string kehlati ha");
